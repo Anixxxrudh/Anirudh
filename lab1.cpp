b0c7d7bb9e97e1edaf20bf9da387e829697d6320
Anixxxrudh <166163822+Anixxxrudh@users.noreply.github.com>
2024-04-05 22:20:04 -0400

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,23 @@
+/*
+Program: First Laboratory
+Author: Anirudh Menon 
+DAte: 1/24/2024
+
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(void) {
+	float width;
+	float length;
+	float area;
+	length = 4.5;
+	width = 3.35;
+	area = length * width;
+	cout << "Rectangle Area = " << area << "\n";
+
+		return 0;
+}
